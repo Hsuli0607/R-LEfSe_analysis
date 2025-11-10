a0a7881ba6a6ce8e5192a16a4eaef5db669aa68d
Hsuli0607 <123087082+Hsuli0607@users.noreply.github.com>
2025-11-10 17:17:49 +0800

    diff_clad-pairwise.r update

--- R/diff_clad-pairwise.r
+++ R/diff_clad-pairwise.r
@@ -10,31 +10,21 @@ library(tidyr)
 library(ape)
 
 
-#' Run LEfSe Analysis
-#'
-#' This function encapsulates the entire LEfSe analysis workflow, from reading
-#' the input data to performing the analysis.
-#'
-#' @param input_path Path to the input CSV file.
-#' @param lda_cutoff LDA score cutoff for LEfSe analysis. Default is 3.
-#' @param strict Strictness of the LEfSe analysis. Default is "1".
-#' @param transform The transformation to apply to the data. Default is "log10p".
-#' @param abundance_filter_threshold Threshold for filtering taxa by abundance. Default is 1000.
-#' @param sample_min_threshold Minimum number of samples a taxon must be present in. Default is 3.
-#' @param curv Whether to use a curved LDA model. Default is TRUE.
-#' @return A `microbiomeMarker` object containing the LEfSe results.
-run_lefse_analysis <- function(input_path = "data/processed/ALL-update.csv",
-                               lda_cutoff = 3,
-                               strict = "1",
-                               transform = "log10p",
-                               rarefy_seed = 394582,
-                               abundance_filter_threshold = 1000,
-                               sample_min_threshold = 3,
-                               multigrp_strat = FALSE,
-                               curv = FALSE) {
 
+### Generate Phyloseq Object Function
+
+#' This function encapsulates the from reading 
+#' the input data to generate phyloseq object.
+#' @param raw_data Path to the input CSV file.
+#' @Return return Phyloseq object
+
+run_generate_physeq <- function(
+                                 raw_data= "data/processed/ALL-update.csv"
+                                 ) {
+  required_pkgs <- c("data.table", "dplyr", "tidyr", "phyloseq", "ape")
+  check_and_load_packages(required_pkgs)
   # 1. Read the data using data.table for speed
-  raw_data_in <- fread(input_path)
+  raw_data_in <- fread(raw_data)
 
   # 2. Separate sample metadata
   sample_info_df <- raw_data_in %>%
@@ -69,30 +59,89 @@ run_lefse_analysis <- function(input_path = "data/processed/ALL-update.csv",
   # Build a phyloseq object
   physeq <- phyloseq(otu_tab, tax_tab, meta_data, phy_tree)
 
-  # Filter taxa before running LEfSe
-  physeq_ab_filtered <- prune_taxa(taxa_sums(physeq) > abundance_filter_threshold, physeq)
-  physeq_filtered <- prune_taxa(rowSums(otu_table(physeq_ab_filtered) > 0) >= 3, physeq_ab_filtered)
-
-  # Run LEfSe analysis
-  lefse_result <- run_lefse(physeq_filtered,
-    group = "Group",
-    transform = transform,
-    lda_cutoff = lda_cutoff,
-    strict = strict,
-    multigrp_strat = multigrp_strat,
-    sample_min = sample_min_threshold,
-    curv = curv
-  )
-  
-  return(lefse_result)
+  return(physeq)
 }
 
-### --- Execute Analysis --- ###
-# Call the function to run the analysis and store the results
-lefse_result <- run_lefse_analysis()
+physeq <- run_generate_physeq()
 
-# Inspect the lefse results marker table:
-marker <- print(marker_table(lefse_result))
+### Subset And Filtering Phyloseq_Object Function
+#'
+#' This function first filters taxa by total abundance and prevalence
+#' across the *entire* dataset, and *then* subsets the samples
+#' based on a specific metadata column and values.
+#'
+#' @param physeq_object The input phyloseq object.
+#' @param class_col A string (character) specifying the name of the 
+#'                  metadata column to use for subsetting (e.g., "Group").
+#' @param subset_vals A vector of values to keep from the `class_col` 
+#'                    (e.g., c("EHI", "EHI.abt")).
+#' @param abundance_filter_threshold The minimum total abundance a taxon 
+#'                                 must have across all samples to be kept.
+#' @param prevalence_filter_threshold The minimum number of samples a taxon
+#'                                    must be present in (count > 0) to be kept.
+#'
+#' @return A new, filtered, and subsetted phyloseq object.
+
+physeq_subset <- function(
+                           physeq_object = run_generate_physeq(),
+                           class_col = "Group",
+                           subset_vals = c("EHI", "EHI.abt"),
+                           abundance_filter_threshold = 1000,
+                           prevalence_filter_threshold = 3
+                         ) {
+  required_pkgs <- c("phyloseq")
+  check_and_load_packages(required_pkgs)
+  
+  # First, filter taxa based on abundance across all samples
+  physeq_ab_filtered <- prune_taxa(taxa_sums(physeq) > abundance_filter_threshold, physeq)
+  
+  # Second, filter out taxa that are not present in at least 3 samples across the full dataset.
+  physeq_prev_filtered <- prune_taxa(rowSums(otu_table(physeq_ab_filtered) > 0) >= prevalence_filter_threshold, physeq_ab_filtered)
+
+  # Finally, subset the fully filtered data to the groups of interest for the pairwise comparison
+  physeq_filtered <- subset_samples(physeq_prev_filtered, class_col %in% subset_vals)
+  
+  return(physeq_filtered)
+}
+
+physeq_filtered <- physeq_subset(physeq)
+
+#' LEfSe Analysis Function
+#'
+#' @param lda_cutoff LDA score cutoff for LEfSe analysis. Default is 3.
+#' @param strict Strictness of the LEfSe analysis. Default is "1".
+#' @param transform The transformation to apply to the data. Default is "log10p".
+#' @param sample_min_threshold Minimum number of samples a taxon must be present in. Default is 3.
+#' @param curv Whether to use a curved LDA model. Default is TRUE.
+#' @return A `microbiomeMarker` object containing the LEfSe results.
+
+### LEfSe Analysis Function
+lefse_analysis <- function(
+                            physeq_object = physeq_filtered,
+                            transform = "log10p",
+                            lda_cutoff = 3,
+                            strict = "1",
+                            multigrp_strat = FALSE,
+                            sample_min_threshold = 3,
+                            curv = FALSE
+                        ) {
+  required_pkgs <- c("microbiomeMarker")
+  check_and_load_packages(required_pkgs)
+  lefse_result <- run_lefse(physeq_object,
+                             transform = transform,
+                             lda_cutoff = lda_cutoff,
+                             strict = strict,
+                             multigrp_strat = multigrp_strat,
+                             sample_min = sample_min_threshold,
+                             curv = curv
+                           )
+return(lefse_result)
+}
+
+lefse_result <- lefse_analysis(physeq_object = physeq)
 
 
 
+
+
+### Plot Diff_Cladogram Function
